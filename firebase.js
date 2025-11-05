ea71120c05a631015ac8005c75212ab604202d52
WoestenNiels <niels.hommey@gmail.com>
2025-11-05 16:47:31 +0100

    Migrate to Firebase SDK v9 with modular imports

--- firebase.js
+++ firebase.js
@@ -1,19 +1,20 @@
-// firebase.js
-// ===> Gebruik dit in combinatie met Firebase SDK v8 (die al in index.html staat)
+// Firebase SDK v9+ (modulaire stijl)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase, ref, set, push, onValue, update } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBD2JdjGaCifjkm90LPKEESZOVCbfyZ9-o",
   authDomain: "de-slimste-mens-finale.firebaseapp.com",
   databaseURL: "https://de-slimste-mens-finale-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "de-slimste-mens-finale",
-  storageBucket: "de-slimste-mens-finale.firebasestorage.app",
+  storageBucket: "de-slimste-mens-finale.appspot.com",
   messagingSenderId: "156720778427",
   appId: "1:156720778427:web:a6e2666746bd95fba06d9e",
   measurementId: "G-LC0H8PJMVN"
 };
 
-// Initialiseer Firebase (v8-stijl)
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
 
-// Verbind met Realtime Database
-const db = firebase.database();
+export { db, ref, set, push, onValue, update };
+
